50bf82696a7cb78c7ea7b0ce4e927019ac7f75af
Mayank-Tiwari <mt1027319@gmail.com>
2023-01-02 23:31:32 +0530

    Slider

--- src/Componets/Home.js
+++ src/Componets/Home.js
@@ -3,12 +3,13 @@ import HotAccessories from './HotAccessories';
 import NavBar from './NavBar';
 import StartProducts from './StartProducts';
 import Slider from './Slider';
+import data from '../data/data.json'
 
 const Home = () => {
   return (
     <div>
     <NavBar/>
-    <Slider/>
+    <Slider start={data.banner.start}/>
     <StartProducts/>
     <HotAccessories/>
     </div>
--- src/Componets/Slider.js
+++ src/Componets/Slider.js
@@ -1,15 +1,17 @@
+import  {Carousel}  from 'react-bootstrap';
 import React from 'react';
-import ImageSlider, {Slide} from 'react-auto-image-slider';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Slider = () => {
-    return (<ImageSlider effectDelay={1000} autoPlayDelay={4000}>
-    <Slide><image src='https://i02.appmifile.com/173_operator_in/09/06/2021/fde866737ea0357305c2d1c852541460.gif' alt='images'/>image 1</Slide>
-    <Slide><image src='https://i02.appmifile.com/615_operator_in/05/07/2021/a4b26d6b2ad2d6d2edbfb23a8482328e.jpg' alt='images'/>Image 2</Slide>
-    <Slide><image src='https://i02.appmifile.com/762_operator_in/05/07/2021/4c90dda893b43e0153950f36ee37dc03.jpg' alt='images'/>Image 3</Slide>
-      
-    </ImageSlider>
-        
-    )
+const Slider = ({start}) => {
+  return (
+    <Carousel>
+      {start.map((item,index)=>(
+        <Carousel.Item>
+      <img className='d-block w-100' src={item} alt='first'/>
+      </Carousel.Item>
+      ))}
+    </Carousel>
+  )
 }
 
-export default Slider
+export default Slider;
