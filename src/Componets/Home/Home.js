0219ae0d3df27e136acda6b0412883ec19682945
Mayank-Tiwari <mt1027319@gmail.com>
2023-01-02 14:19:40 +0530

    Slider

--- src/Componets/Home/Home.js
+++ src/Componets/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from '../NavBar';
+import Slider from './Slider';
 
 const Home = () => {
   return (
     <div>
     <NavBar/>
+    <Slider/>
     </div>
   )
 }
