48f1cdf20b6591168e189b64b85e3714ca821f53
Mayank-Tiwari <mt1027319@gmail.com>
2023-01-12 16:45:01 +0530

    mobile

--- src/Componets/Mobile.js
+++ src/Componets/Mobile.js
@@ -1,8 +1,40 @@
-import React from 'react'
+import React from 'react';
+import data from '../data/data.json';
+import '../Styles/mobile.css';
 
+const mobile = data.mobiles;
 const Mobile = () => {
   return (
-    <div>Mobile</div>
+    <div>
+      {mobile.map((item)=>(
+        <div className='main-container'>
+        <div className='product-image-div'>
+          <img src={item.image} alt='mobile'/>
+          <input type='checkbox'/>Add to Compare
+        </div>
+        <div className='product-div'>
+          <h5>{item.name}</h5>
+          <ul>
+            <li>{item.storage}</li>
+            <li>{item.screen}</li>
+            <li>{item.camera}</li>
+            <li>{item.battery}</li>
+            <li>{item.processor}</li>
+            <li>{item.warranty}</li>
+          </ul>
+        </div>
+        <div>
+          <h4>{item.price}</h4>
+          <ul>
+            <li>{item.offer}</li>
+            <li>{item.delivery}</li>
+            <li>{item.Exchange}</li>
+            <li>{item.Emi}</li>
+          </ul>
+        </div>
+      </div>
+      ))}
+    </div>
   )
 }
 
--- src/Componets/NavBar.js
+++ src/Componets/NavBar.js
@@ -1,22 +1,16 @@
 import React from 'react'
 import { Link, Outlet } from 'react-router-dom';
 import '../Styles/navBar.css';
-import Accessories from './Accessories';
-import Laptop from './Laptop';
-import Mobile from './Mobile';
-import Tablet from './Tablet';
-import TopOffers from './TopOffers';
-import TV from './TV';
 
 const NavBar = () => {
   return (
     <div className='navBar'>
-    <Link className='navbarlink' to='/mobile' ><Mobile/></Link>
-    <Link className='navbarlink' to='/laptop' ><Laptop/></Link>
-    <Link className='navbarlink' to='/tv' ><TV/></Link>
-    <Link className='navbarlink' to='/tablet' ><Tablet/></Link>
-    <Link className='navbarlink' to='/accessories' ><Accessories/></Link>
-    <Link className='navbarlink' to='/top-offers' ><TopOffers/></Link>
+    <Link className='navbarlink' to='/mobile' >Mobile</Link>
+    <Link className='navbarlink' to='/laptop' >Laptop</Link>
+    <Link className='navbarlink' to='/tv' >TV</Link>
+    <Link className='navbarlink' to='/tablet' >Tablet</Link>
+    <Link className='navbarlink' to='/accessories' >Accessories</Link>
+    <Link className='navbarlink' to='/top-offers' >TopOffers</Link>
     <Outlet/>
     </div>
   )
